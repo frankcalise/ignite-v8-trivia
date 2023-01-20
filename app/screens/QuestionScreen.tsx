04b3e2cd2fed746375a6ef0a183f6a9d5ce3f385
Frank Calise <fcalise@gmail.com>
2023-01-20 15:42:35 -0500

    updated question screen header

--- app/screens/QuestionScreen.tsx
+++ app/screens/QuestionScreen.tsx
@@ -1,9 +1,10 @@
 import React, { FC } from "react"
 import { observer } from "mobx-react-lite"
-import { ViewStyle } from "react-native"
+import { TextStyle, ViewStyle } from "react-native"
 import { StackScreenProps } from "@react-navigation/stack"
 import { AppStackScreenProps } from "../navigators"
 import { Screen, Text } from "../components"
+import { spacing } from "../theme"
 // import { useNavigation } from "@react-navigation/native"
 // import { useStores } from "../models"
 
@@ -16,19 +17,27 @@ import { Screen, Text } from "../components"
 
 // REMOVE ME! ⬇️ This TS ignore will not be necessary after you've added the correct navigator param type
 // @ts-ignore
-export const QuestionScreen: FC<StackScreenProps<AppStackScreenProps, "Question">> = observer(function QuestionScreen() {
-  // Pull in one of our MST stores
-  // const { someStore, anotherStore } = useStores()
+export const QuestionScreen: FC<StackScreenProps<AppStackScreenProps, "Question">> = observer(
+  function QuestionScreen() {
+    // Pull in one of our MST stores
+    // const { someStore, anotherStore } = useStores()
 
-  // Pull in navigation via hook
-  // const navigation = useNavigation()
-  return (
-    <Screen style={$root} preset="scroll">
-      <Text text="question" />
-    </Screen>
-  )
-})
+    // Pull in navigation via hook
+    // const navigation = useNavigation()
+    return (
+      <Screen style={$root} preset="fixed">
+        <Text preset="heading" text="Question" style={$header} />
+      </Screen>
+    )
+  },
+)
 
 const $root: ViewStyle = {
   flex: 1,
+  paddingHorizontal: spacing.large,
+}
+
+const $header: TextStyle = {
+  marginTop: spacing.huge,
+  marginBottom: spacing.medium,
 }
